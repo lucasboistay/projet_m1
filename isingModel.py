456d7808d8f9535a5cf3cca37c5484e5a8134d02
Lucas BOISTAY <lucasboistay@hotmail.fr>
2024-02-28 19:02:16 +0100

    Là c'est mieux déjà

--- isingModel.py
+++ isingModel.py
@@ -1,10 +1,12 @@
 from scipy import constants
 import numpy as np
-
+from multiprocessing import Pool
+from functools import partial
 # Physical constants
 
 K = constants.k
 J = 1  # Energy unit
+
 
 # Ising model class
 
@@ -22,14 +24,12 @@ class IsingModel:
     run_monte_carlo: Run Monte Carlo simulation
     """
 
-
-
-    def __init__(self, M=10, N=10, T=300, iteration=5000):
-        self.M = M # Number of rows
-        self.N = N # Number of columns
-        self.T = T # Temperature
-        self.iteration = iteration # Number of iterations
-        self.beta = 1 / (K * T) # Beta
+    def __init__(self, M=10, N=10, T=300, iteration=1000):
+        self.M = M  # Number of rows
+        self.N = N  # Number of columns
+        self.T = T  # Temperature
+        self.iteration = iteration  # Number of iterations
+        self.beta = 1 / (K * T)  # Beta
         self.lattice = np.zeros((M, N))
 
         self.energy_history = []
@@ -44,6 +44,10 @@ class IsingModel:
         # Initialize lattice for different types
         if lattice == "random":
             self.lattice = np.random.choice([1, -1], (self.M, self.N))
+
+            # periodic boundary conditions
+            self.lattice[0, :] = self.lattice[-1, :]
+            self.lattice[:, 0] = self.lattice[:, -1]
         elif lattice == 1:
             self.lattice = np.ones((self.M, self.N))
         elif lattice == -1:
@@ -54,15 +58,18 @@ class IsingModel:
             raise ValueError("Invalid lattice type")
         return self.lattice
 
+    def get_lattice(self):
+        return self.lattice
+
     def print_lattice(self):
         print(self.lattice)
 
     def energy(self, i, j):
-        return -J * self.lattice[i, j] * (
-            self.lattice[(i + 1) % self.M, j] +
-            self.lattice[i, (j + 1) % self.N] +
-            self.lattice[(i - 1) % self.M, j] +
-            self.lattice[i, (j - 1) % self.N]
+        return J * self.lattice[i, j] * (
+                self.lattice[(i + 1) % self.M, j] +
+                self.lattice[i, (j + 1) % self.N] +
+                self.lattice[(i - 1) % self.M, j] +
+                self.lattice[i, (j - 1) % self.N]
         )
 
     def get_total_energy(self):
@@ -70,31 +77,52 @@ class IsingModel:
         for i in range(self.M):
             for j in range(self.N):
                 energy += self.energy(i, j)
-        return energy
+        return -1 * energy
 
     def magnetization(self):
         return np.sum(self.lattice)
 
+    def flip_spin(self, i, j):
+        self.lattice[i, j] *= -1
+
+        # Periodic boundary conditions
+
+        if i == 0:
+            self.lattice[self.M - 1, j] = self.lattice[i, j]  # Top
+        if i == self.M - 1:
+            self.lattice[0, j] = self.lattice[i, j]  # Bottom
+        if j == 0:
+            self.lattice[i, self.N - 1] = self.lattice[i, j]  # Left
+        if j == self.N - 1:
+            self.lattice[i, 0] = self.lattice[i, j]  # Right
+
     def monte_carlo_step(self):
-        i, j = np.random.randint(0, self.M), np.random.randint(0, self.N) # Randomly select a site
+        i, j = np.random.randint(0, self.M), np.random.randint(0, self.N)  # Randomly select a site
 
-        energy_before = 2*self.energy(i,j) # Calculate energy before flipping the spin
-        self.lattice[i, j] *= -1 # Flip the spin
-        energy_after = 2*self.energy(i,j) # Calculate energy after flipping the spin
-        dE = energy_after - energy_before # Calculate the change in energy
+        energy_before = 2 * self.energy(i, j)  # Calculate energy before flipping the spin
+        self.flip_spin(i, j)  # Flip the spin
+        energy_after = 2 * self.energy(i, j)  # Calculate energy after flipping the spin
+        dE = energy_after - energy_before  # Calculate the change in energy
 
-        if dE < 0 or np.random.random() < np.exp(-dE * self.beta): # Accept the flip with a certain probability
+        if dE < 0 or np.random.random() < np.exp(-dE * self.beta):  # Accept the flip with a certain probability
             pass
-        else: # Reject the flip
-            self.lattice[i, j] *= -1 # Flip the spin back
-
-        self.energy_history.append(self.get_total_energy())
-        self.magnetization_history.append(self.magnetization())
+        else:  # If the flip is not accepted, flip the spin back
+            self.flip_spin(i, j)
 
     def run_monte_carlo(self):
         for _ in range(self.iteration):
             self.monte_carlo_step()
+            self.energy_history.append(self.get_total_energy())
+            self.magnetization_history.append(self.magnetization())
         return self.lattice
+
+    def parallel_monte_carlo_step(self, M, N, beta, iteration):
+        # TODO: Implement parallel monte carlo step
+        pass
+
+    def run_parallel_monte_carlo(self):
+        # TODO Implement parallel monte carlo
+        pass
 
     def get_energy_history(self):
         return self.energy_history
--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-
 ## main.py
 
 from isingModel import IsingModel
 import matplotlib.pyplot as plt
+import time
+import numpy as np
 
-ising = IsingModel()
-#ising.initialize_lattice("random") # To get a random lattice
+print("---- Creating the lattice ----")
+ising = IsingModel(50, 50, 300, 1000)
+# ising.initialize_lattice("random") # To get a random lattice
 ising.initialize_lattice(1) # To get a lattice with all 1's
-#ising.initialize_lattice(-1) # To get a lattice with all -1's
-#ising.initialize_lattice(0) # To get a lattice with all 0's
+# ising.initialize_lattice(-1)  # To get a lattice with all -1's
+# ising.initialize_lattice(0) # To get a lattice with all 0's
 
 # information about the lattice
-ising.print_lattice()
-print("---- Creating the lattice ----")
+starting_lattice = np.copy(ising.get_lattice())
+
 print(f"Total Energy {ising.get_total_energy()}")
 print(f"Total Magnetization {ising.magnetization()}")
 
 # Run Monte Carlo simulation
 print("---- Running Monte Carlo simulation ----")
+start = time.time()
 ising.run_monte_carlo()
+end = time.time()
 print("Simolation completed")
 print(f"New total Energy {ising.get_total_energy()}")
 print(f"New total Magnetization {ising.magnetization()}")
-ising.print_lattice()
+print(f"Time taken {end - start} seconds")
+ending_lattice = np.copy(ising.get_lattice())
 
 # Plot the energy and magnetization
 
@@ -31,4 +36,20 @@ plt.plot(ising.magnetization_history, label="Magnetization")
 plt.xlabel("Iteration")
 plt.ylabel("Energy")
 plt.legend()
+plt.savefig("data/energy_magnetization.png")
 plt.show()
+
+# Plot the lattice with heat map on the same figure
+
+plt.figure()
+plt.subplot(1, 2, 1)
+plt.imshow(starting_lattice, cmap="hot")
+plt.title("Initial lattice")
+
+plt.subplot(1, 2, 2)
+plt.imshow(ending_lattice, cmap="hot")
+plt.title("Final lattice")
+plt.show()
+
+plt.savefig("data/lattice.png")
+
