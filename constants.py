01c8a10a113d57e48a4a28f48e0e48d966a2ebcf
Lucas BOISTAY <lucasboistay@hotmail.fr>
2024-02-29 17:10:03 +0100

    README changes

--- constants.py
+++ constants.py
@@ -2,16 +2,16 @@
 
 # General model constants
 
-N = 100
-M = 100
-iterations = 100000
+N = 100 # Number of rows
+M = 100 # Number of columns
+iterations = 100000 # Number of iterations
 
 # Parallel processing constants
 
-number_of_simulations = 100
+number_of_simulations = 100 # Number of simulations for the critical temperature computation
 number_of_pool_processes = 10  # Number of pool processes, do not set it to more than the number of cores of your CPU
 
 # Temperature constants
 
-t_min = 0.1
-t_max = 4
+t_min = 0.1 # Minimum temperature for the critical temperature computation
+t_max = 4 # Maximum temperature for the critical temperature computation
